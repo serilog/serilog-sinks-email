bccb3d05ab34eb1e921deaf62b940511811023b3
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2015-03-07 15:04:59 +1000

    Moved source from serilog/serilog (no build yet)

---
+++ src/Serilog.Sinks.Email/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Serilog.Sinks.Email")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2014")]

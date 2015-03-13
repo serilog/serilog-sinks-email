09cbe679b0dca71eff979cdff8480cf01aa74458
Matthew Erbs <matthew.erbs@merbla.com>
2015-03-14 08:30:16 +1000

    Added files for AppVeyor

---
+++ Build.ps1
@@ -0,0 +1,74 @@
+param(
+    [String] $majorMinor = "0.0",  # 2.0
+    [String] $patch = "0",         # $env:APPVEYOR_BUILD_VERSION
+    [String] $customLogger = "",   # C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll
+    [Switch] $notouch,
+    [String] $sln                  # e.g serilog-sink-name.sln
+)
+
+function Set-AssemblyVersions($informational, $assembly)
+{
+    (Get-Content assets/CommonAssemblyInfo.cs) |
+        ForEach-Object { $_ -replace """1.0.0.0""", """$assembly""" } |
+        ForEach-Object { $_ -replace """1.0.0""", """$informational""" } |
+        ForEach-Object { $_ -replace """1.1.1.1""", """$($informational).0""" } |
+        Set-Content assets/CommonAssemblyInfo.cs
+}
+
+function Install-NuGetPackages($solution)
+{
+    nuget restore $solution
+}
+
+function Invoke-MSBuild($solution, $customLogger)
+{
+    if ($customLogger)
+    {
+        msbuild "$solution" /verbosity:minimal /p:Configuration=Release /logger:"$customLogger"
+    }
+    else
+    {
+        msbuild "$solution" /verbosity:minimal /p:Configuration=Release
+    }
+}
+
+function Invoke-NuGetPackProj($csproj)
+{
+    nuget pack -Prop Configuration=Release -Symbols $csproj
+}
+
+function Invoke-NuGetPackSpec($nuspec, $version)
+{
+    nuget pack $nuspec -Version $version -OutputDirectory ..\..\
+}
+
+function Invoke-NuGetPack($version)
+{
+    ls src/**/*.csproj |
+        Where-Object { -not ($_.Name -like "*net40*") } |
+        ForEach-Object { Invoke-NuGetPackProj $_ }
+}
+
+function Invoke-Build($majorMinor, $patch, $customLogger, $notouch, $sln)
+{
+    $package="$majorMinor.$patch"
+
+    Write-Output "Building Serilog.Sinks.Loggly $package"
+
+    if (-not $notouch)
+    {
+        $assembly = "$majorMinor.0.0"
+
+        Write-Output "Assembly version will be set to $assembly"
+        Set-AssemblyVersions $package $assembly
+    }
+
+    Install-NuGetPackages $sln
+    
+    Invoke-MSBuild $sln $customLogger
+
+    Invoke-NuGetPack $package
+}
+
+$ErrorActionPreference = "Stop"
+Invoke-Build $majorMinor $patch $customLogger $notouch $sln
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # serilog-sinks-email
 A Serilog sink that writes events to SMTP email
+
+ 

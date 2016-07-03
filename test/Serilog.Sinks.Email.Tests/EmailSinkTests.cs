54d5699eead41947373e6cd8868a41a6d4f96df2
Adam Chester <adamchester@gmail.com>
2016-07-03 16:27:20 +1000

    Add tests project

---
+++ test/Serilog.Sinks.Email.Tests/EmailSinkTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Serilog.Debugging;
+using Xunit;
+
+namespace Serilog.Sinks.Email.Tests
+{
+    public class EmailSinkTests
+    {
+        [Fact(Skip="Requires a localhost mail server")]
+        public void Works()
+        {
+            var selfLogMessages = new List<string>();
+            SelfLog.Enable(selfLogMessages.Add);
+
+            var emailLogger = new LoggerConfiguration()
+                .WriteTo.Email(
+                    fromEmail: "from@localhost.local",
+                    toEmail: "to@localhost.local",
+                    mailServer: "localhost",
+                    outputTemplate: "[{Level}] {Message}{NewLine}{Exception}",
+                    mailSubject: "subject")
+                .CreateLogger();
+
+            emailLogger.Information("test {test}", "test");
+            emailLogger.Dispose();
+
+            Assert.Equal(Enumerable.Empty<string>(), selfLogMessages);
+        }
+    }
+}

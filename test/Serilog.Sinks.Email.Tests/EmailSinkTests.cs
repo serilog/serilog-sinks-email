68d686157bca0dbe565556678d02e4cfadd7f84e
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2018-07-12 16:25:34 +0200

    Only take the first line of the subject

--- test/Serilog.Sinks.Email.Tests/EmailSinkTests.cs
+++ test/Serilog.Sinks.Email.Tests/EmailSinkTests.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Serilog.Debugging;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using Serilog.Parsing;
 using Xunit;
 
 namespace Serilog.Sinks.Email.Tests
@@ -26,6 +30,24 @@ namespace Serilog.Sinks.Email.Tests
             }
 
             Assert.Equal(Enumerable.Empty<string>(), selfLogMessages);
+        }
+
+        [Fact]
+        public void MultilineMessageCreatesSubjectWithTheFirstLineOnly()
+        {
+            var subjectLineFormatter = new MessageTemplateTextFormatter("{Message}", null);
+
+            var logEvents = new[]
+            {
+                new LogEvent(DateTimeOffset.Now, LogEventLevel.Error, new Exception("An exception occured"),
+                    new MessageTemplate(@"Subject",
+                        new MessageTemplateToken[]{new PropertyToken("Message", "A multiline" + Environment.NewLine + "Message")})
+                    //        Enumerable.Empty<MessageTemplateToken>())
+                    , Enumerable.Empty<LogEventProperty>())
+            };
+            var mailSubject = EmailSink.ComputeMailSubject(subjectLineFormatter, logEvents);
+
+            Assert.Equal("A multiline", mailSubject);
         }
     }
 }

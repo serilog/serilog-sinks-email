c0e885051a5af248ab305d5ac39eeda31a862009
Cédric Luthi <cedric.luthi@gmail.com>
2022-02-03 08:19:13 +0100

    Apply .editorconfig formatting (trim_trailing_whitespace = true)

--- src/Serilog.Sinks.Email/Sinks/Email/EmailSink.cs
+++ src/Serilog.Sinks.Email/Sinks/Email/EmailSink.cs
@@ -1,11 +1,11 @@
 // Copyright 2014 Serilog Contributors
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -49,7 +49,7 @@ namespace Serilog.Sinks.Email
             _textFormatter = textFormatter;
             _subjectLineFormatter = subjectLineFormatter;
         }
-        
+
         /// <summary>
         /// Emit a batch of log events, running asynchronously.
         /// </summary>

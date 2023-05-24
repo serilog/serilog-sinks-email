25420942bce2598d0799353e4f6cae38ed3a5e84
Kamil Ściana <kamil.sciana@kruksa.pl>
2023-05-24 10:43:37 +0200

    feat: update Ubuntu build

--- build.sh
+++ build.sh
@@ -10,14 +10,11 @@ dotnet restore
 
 echo "🤖 Attempting to build..."
 for path in src/**/*.csproj; do
-    dotnet build -f netstandard1.3 -c Release ${path}
     dotnet build -f netstandard2.0 -c Release ${path}
-    dotnet build -f netstandard2.1 -c Release ${path}
 done
 
 echo "🤖 Running tests..."
 for path in test/*.Tests/*.csproj; do
-    dotnet test -f netcoreapp2.2  -c Release ${path}
-    dotnet test -f netcoreapp3.1  -c Release ${path}
+    dotnet test -f net6.0  -c Release ${path}
 done
 
